25c4419cd8102b7d2bcda2fc8b11fdb6007fa05a
Joel Anton <joel@novu.co>
2024-03-08 11:31:26 -0800

    feat: Populate menu items

---
+++ apps/web/src/components/nav/RootNavMenu.tsx
@@ -0,0 +1,139 @@
+import {
+  IconAutorenew,
+  IconCellTower,
+  IconDomain,
+  IconGroup,
+  IconOutlineMonitorHeart,
+  IconRoute,
+  IconSettings,
+  IconTaskAlt,
+  IconTranslate,
+  IconViewQuilt,
+} from '@novu/design-system';
+import { UTM_CAMPAIGN_QUERY_PARAM } from '@novu/shared';
+import { ROUTES } from '@novu/shared-web';
+import { HStack } from '../../styled-system/jsx';
+import { EnvironmentSelect } from './EnvironmentSelect';
+import { NavMenu } from './NavMenu';
+import { NavMenuLinkButton } from './NavMenuButton';
+import { NavMenuSection } from './NavMenuSection';
+import { NavOrganizationSelect } from './NavOrganizationSelect';
+
+export const RootNavMenu: React.FC = () => {
+  return (
+    <NavMenu variant="root">
+      <NavMenuSection>
+        <NavOrganizationSelect />
+        <NavMenuLinkButton
+          label="Get started"
+          isVisible={true}
+          icon={<IconTaskAlt />}
+          link={ROUTES.GET_STARTED}
+          // rightSide: { component: <VisibilityOff onClick={handleHideOnboardingClick} /> displayOnHover: true }} />}
+          testId="side-nav-quickstart-link"
+          // tooltipLabel: 'Hide this page from menu',
+        />
+        <NavMenuLinkButton
+          icon={<IconCellTower />}
+          link={ROUTES.INTEGRATIONS}
+          label="Integrations"
+          testId="side-nav-integrations-link"
+        />
+        <NavMenuLinkButton
+          label="Settings"
+          icon={<IconSettings />}
+          link={ROUTES.SETTINGS}
+          testId="side-nav-settings-link"
+        />
+      </NavMenuSection>
+      <NavMenuSection>
+        <EnvironmentSelect />
+        <NavMenuLinkButton
+          label="Workflows"
+          icon={<IconRoute />}
+          link={ROUTES.WORKFLOWS}
+          testId="side-nav-templates-link"
+        />
+        <NavMenuLinkButton
+          icon={<IconOutlineMonitorHeart />}
+          link={ROUTES.ACTIVITIES}
+          label="Activity Feed"
+          testId="side-nav-activities-link"
+        />
+        <NavMenuLinkButton
+          label="Change history"
+          icon={<IconAutorenew />}
+          link={ROUTES.CHANGES}
+          testId={'side-nav-changes-link'}
+          // rightSide: <ChangesCountBadge />}
+          isVisible={true}
+        />
+        <NavMenuLinkButton
+          label="Subscribers"
+          icon={<IconGroup />}
+          link={ROUTES.SUBSCRIBERS}
+          testId="side-nav-subscribers-link"
+        />
+        <NavMenuLinkButton
+          label="Tenants"
+          isVisible={true}
+          icon={<IconDomain />}
+          link={ROUTES.TENANTS}
+          testId="side-nav-tenants-link"
+        />
+        <NavMenuLinkButton label="Layouts" icon={<IconViewQuilt />} link={'/todo'} testId="side-nav-settings-link" />
+        <NavMenuLinkButton
+          label="Translations"
+          isVisible={true}
+          icon={<IconTranslate width={20} height={20} />}
+          link={ROUTES.TRANSLATIONS}
+          testId="side-nav-translations-link"
+        />
+      </NavMenuSection>
+      <RootNavMenuFooter />
+    </NavMenu>
+  );
+};
+
+function RootNavMenuFooter() {
+  return (
+    <HStack
+      textStyle={'text.secondary'}
+      color="typography.text.secondary"
+      data-test-id="side-nav-root-footer"
+      position="fixed"
+      bottom={'100'}
+    >
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://discord.novu.co"
+        data-test-id="side-nav-bottom-link-support"
+      >
+        Support
+      </a>
+      <p>
+        <b>&nbsp;&nbsp;&nbsp;•&nbsp;&nbsp;&nbsp;</b>
+      </p>
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href={`https://docs.novu.co${UTM_CAMPAIGN_QUERY_PARAM}`}
+        data-test-id="side-nav-bottom-link-documentation"
+      >
+        Docs
+      </a>
+      <p>
+        <b>&nbsp;&nbsp;&nbsp;•&nbsp;&nbsp;&nbsp;</b>
+      </p>
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://github.com/novuhq/novu/issues/new/choose"
+        data-test-id="side-nav-bottom-link-share-feedback"
+      >
+        Share Feedback
+      </a>
+    </HStack>
+  );
+}
---
+++ apps/web/src/components/nav/SettingsNavMenu.tsx
@@ -0,0 +1,101 @@
+import {
+  IconManageAccounts,
+  IconRoomPreferences,
+  IconAdminPanelSettings,
+  IconGroup,
+  IconWorkspacePremium,
+  IconCreditCard,
+  IconConstruction,
+  IconKey,
+  IconWebhook,
+} from '@novu/design-system';
+import { NavMenu } from './NavMenu';
+import { NavMenuLinkButton, NavMenuToggleButton } from './NavMenuButton';
+import { NavMenuSection } from './NavMenuSection';
+
+export const SettingsNavMenu: React.FC = () => {
+  return (
+    <NavMenu variant="nested" title="Settings">
+      <NavMenuSection title="Account">
+        <NavMenuLinkButton
+          label="User profile"
+          isVisible
+          icon={<IconManageAccounts />}
+          link={'/workflows'}
+          testId="side-nav-settings-user-profile"
+        ></NavMenuLinkButton>
+      </NavMenuSection>
+      <NavMenuSection title="Organization <ORG NAME>">
+        <NavMenuLinkButton
+          label="Organization profile"
+          isVisible
+          icon={<IconRoomPreferences />}
+          link={'/todo'}
+          testId="side-nav-settings-translations-link"
+        ></NavMenuLinkButton>
+        <NavMenuLinkButton
+          label="Access security"
+          isVisible
+          icon={<IconAdminPanelSettings />}
+          link={'/todo'}
+          testId="side-nav-settings-translations-link"
+        ></NavMenuLinkButton>
+        <NavMenuLinkButton
+          label="Team members"
+          isVisible
+          icon={<IconGroup />}
+          link={'/todo'}
+          testId="side-nav-settings-translations-link"
+        ></NavMenuLinkButton>
+        <NavMenuLinkButton
+          label="Branding"
+          isVisible
+          icon={<IconWorkspacePremium />}
+          link={'/todo'}
+          testId="side-nav-settings-translations-link"
+        ></NavMenuLinkButton>
+        <NavMenuLinkButton
+          label="Billing plans"
+          isVisible
+          icon={<IconCreditCard />}
+          link={'/todo'}
+          testId="side-nav-settings-translations-link"
+        ></NavMenuLinkButton>
+      </NavMenuSection>
+      <NavMenuSection title="Environments <ORG NAME>">
+        <NavMenuToggleButton icon={<IconConstruction />} label={'Development'} testId="side-nav-settings-development">
+          <NavMenuLinkButton
+            label="API keys"
+            isVisible
+            icon={<IconKey />}
+            link={'/todo'}
+            testId="side-nav-settings-development-api-keys"
+          ></NavMenuLinkButton>
+          <NavMenuLinkButton
+            label="Inbound webhook"
+            isVisible
+            icon={<IconWebhook />}
+            link={'/todo'}
+            testId="side-nav-settings-development-inbound-webhook"
+          ></NavMenuLinkButton>
+        </NavMenuToggleButton>
+        <NavMenuToggleButton icon={<IconConstruction />} label={'Production'} testId="side-nav-settings-production">
+          <NavMenuLinkButton
+            label="API keys"
+            isVisible
+            icon={<IconKey />}
+            link={'/todo'}
+            testId="side-nav-settings-production-api-keys"
+          ></NavMenuLinkButton>
+          <NavMenuLinkButton
+            label="Inbound webhook"
+            isVisible
+            icon={<IconWebhook />}
+            link={'/todo'}
+            testId="side-nav-settings-production-inbound-webhook"
+          ></NavMenuLinkButton>
+        </NavMenuToggleButton>
+      </NavMenuSection>
+    </NavMenu>
+  );
+};
