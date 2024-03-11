7af0fc808d9c88a1e6329e8559f8a1282d638a26
Joel Anton <joel@novu.co>
2024-03-11 08:10:55 -0700

    chore: styling

--- apps/web/src/components/nav/NavOrganizationSelect.tsx
+++ apps/web/src/components/nav/NavOrganizationSelect.tsx
@@ -1,35 +1,60 @@
-import { IconRocketLaunch, Select } from '@novu/design-system';
+import { Select } from '@novu/design-system';
 import { css } from '../../styled-system/css';
 import { useOrganizationSelect } from '../layout/components/OrganizationSelect';
 
-export const NavOrganizationSelect = () => {
-  const selectProps = useOrganizationSelect();
+interface INavOrganizationSelectRendererProps {
+  loadingAddOrganization: boolean;
+  loadingSwitch: boolean;
+  addOrganizationItem: (newOrganization: string) => undefined;
+  value: string;
+  switchOrgCallback: (organizationId: string | string[] | null) => Promise<void>;
+  setSearch: React.Dispatch<React.SetStateAction<string>>;
+  data: {
+    label: string;
+    value: string;
+  }[];
+}
 
+export const NavOrganizationSelectRenderer: React.FC<INavOrganizationSelectRendererProps> = ({
+  loadingAddOrganization,
+  loadingSwitch,
+  addOrganizationItem,
+  value,
+  switchOrgCallback,
+  setSearch,
+  data,
+}) => {
   return (
     <Select
       data-test-id="organization-switch"
       className={css({ '& input': { bg: 'transparent', border: 'none !important' } })}
       creatable
       searchable
+      loading={loadingAddOrganization || loadingSwitch}
       getCreateLabel={(newOrganization) => <div>+ Add "{newOrganization}"</div>}
+      onCreate={addOrganizationItem}
+      value={value}
+      onChange={switchOrgCallback}
       allowDeselect={false}
+      onSearchChange={setSearch}
+      data={data}
       icon={
-        <span
+        <img
+          // TODO: use actual organization photo
+          src="https://assets.super.so/1e9f5a51-c4c6-4fca-b6e8-25fa0186f139/uploads/favicon/d82d95bb-0983-4980-8b3f-dda6ecb0c22c.png"
           className={css({
-            p: '50',
+            w: '200',
+            h: '200',
             mr: '100',
             // TODO: use design system values when available
             borderRadius: '8px',
-            bg: 'surface.page',
-            '& svg': {
-              fill: 'typography.text.main',
-            },
           })}
-        >
-          <IconRocketLaunch />
-        </span>
+        />
       }
-      {...selectProps}
     />
   );
+};
+
+export const NavOrganizationSelect = () => {
+  return <NavOrganizationSelectRenderer {...useOrganizationSelect()} />;
 };
